1df0190e793b3c45a4d12ad0947184177653c89b
shradercinc <82776089+shradercinc@users.noreply.github.com>
2022-12-01 14:00:35 -0500

    inventory slots

--- Vampires-In-Louisiana/Assets/Scripts/Inventory/Icon.cs
+++ Vampires-In-Louisiana/Assets/Scripts/Inventory/Icon.cs
@@ -2,18 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class Icon : MonoBehaviour
+public class Icon : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public GameObject pl;
+    private Transform pos;
     private Image img;
     public Sprite Txr;
+    private Vector3 home;
+    private bool isOver = false;
+    private bool isDrag = false;
+    private bool locked = false;
+    private bool touchBody = false;
+    private Vector3 bPos;
+    public GraphicRaycaster GRay;
     // Start is called before the first frame update
     void Start()
     {
+
+        pos = GetComponent<Transform>();
         pl = GameObject.FindGameObjectWithTag("Player");
+        GRay = GameObject.FindGameObjectWithTag("Board").GetComponent<GraphicRaycaster>();
         img = GetComponent<Image>();
         img.sprite = Txr;
+        home = pos.position;
+    }
+
+    public void OnPointerEnter(PointerEventData data)
+    {
+        isOver = true;
+    }
+    public void OnPointerExit(PointerEventData data)
+    {
+        isOver = false;
     }
 
     // Update is called once per frame
@@ -23,6 +45,42 @@ public class Icon : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.I))
         {
             Destroy(this.gameObject);
-        }    
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && isOver == true && locked == false)
+        {
+            pos.position = Input.mousePosition;
+            isDrag = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            var eventdata = new PointerEventData(null);
+            eventdata.position = Input.mousePosition;
+            var resualts = new List<RaycastResult>();
+            GRay.Raycast(eventdata, resualts);
+            foreach (var resualt in resualts)
+            {
+                if (resualt.gameObject.CompareTag("Body"))
+                {
+                    touchBody = true;
+                    bPos = resualt.gameObject.GetComponent<Transform>().position;
+                    resualt.gameObject.GetComponent<Image>().sprite = img.sprite;
+                    break;
+                }
+            }
+            if (touchBody == true)
+            {
+                pos.position = bPos;
+                locked = true;
+            } else pos.position = home;
+            isDrag = false;
+        }
+
+        if (isDrag == true)
+        {
+            pos.position = Input.mousePosition;
+        }
+
     }
 }
